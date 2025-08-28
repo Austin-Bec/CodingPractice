9385b7106c25597730109edf6946be320cf536a5
Austin Beck <abeck23@my.gcu.edu>
2025-08-28 10:07:08 -0500

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+
+color = input("What is your favorite color? ")
+
+age = int(input("How old are you? "))
+
+print(f"It's nice to meet you {name}! {color.capitalize()} is a great color. By next year you will be", age + 1)
